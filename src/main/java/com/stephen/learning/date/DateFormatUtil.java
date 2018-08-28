f4d68eecd0dbbd5d8443c246e51cbce23628f7f8
jack <zhangkun@51dojo.com>
2018-08-28 23:14:58 +0800

    增加了时间处理操作

---
+++ src/main/java/com/stephen/learning/date/DateFormatUtil.java
@@ -0,0 +1,115 @@
+package com.stephen.learning.date;
+
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Splitter;
+import com.stephen.learning.enumeration.MonthConstant;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @Auther: jack
+ * @Date: 2018/8/28 10:20
+ * @Description: 时间处理
+ */
+public class DateFormatUtil {
+    private static String PATTERN="yyyy-MM-dd HH:mm";
+
+    /**
+     * 时间转换
+     * @param callTime 原始时间格式：2 Agustus 11.39
+     * @param timestamp 时间戳
+     * @return
+     */
+    public static String getTime(String callTime,String timestamp){
+        //使用guava字符串分割
+        List<String> data=Splitter.on(CharMatcher.is(' ').or(CharMatcher.is('.')))
+                .splitToList(callTime);
+
+        /*List<String> data2=Splitter.on(new CharMatcher() {
+            @Override
+            public boolean matches(char c) {
+                return c==' '||c=='.';
+            }
+        }).splitToList(callTime);*/
+        Date date=new Date(Long.valueOf(timestamp));
+        Calendar calendar=Calendar.getInstance();
+        calendar.setTime(date);
+        int year=calendar.get(Calendar.YEAR);
+        int month=MonthConstant.Indonesian.valueOf(data.get(1)).getMonth()-1;
+        int day=Integer.valueOf(data.get(0));
+        int hour=Integer.valueOf(data.get(2));
+        int minute=Integer.valueOf(data.get(3));
+        calendar.set(year,month,day,hour,minute);
+        //使用apache时间格式化工具
+        return DateFormatUtils.format(calendar.getTime(),PATTERN);
+    }
+
+    /**
+     *
+     * @param callTime 原始时间格式：August 2, 11:39
+     * @param timestamp
+     * @return
+     */
+    public static String getTime2(String callTime,String timestamp){
+        List<String> data=Splitter.on(CharMatcher.is(' ').or(CharMatcher.is(',')).or(CharMatcher.is(':'))).
+                //去掉空字符串
+                omitEmptyStrings().
+                splitToList(callTime);
+        Date date=new Date(Long.valueOf(timestamp));
+        Calendar calendar=Calendar.getInstance();
+        calendar.setTime(date);
+        int year=calendar.get(Calendar.YEAR);
+        int month=MonthConstant.English.valueOf(data.get(0)).getMonth()-1;
+        int day=Integer.valueOf(data.get(1));
+        int hour=Integer.valueOf(data.get(2));
+        int minute=Integer.valueOf(data.get(3));
+        calendar.set(year,month,day,hour,minute);
+        //使用apache时间格式化工具
+        return DateFormatUtils.format(calendar.getTime(),PATTERN);
+    }
+
+    /**
+     *
+     * @param callTime 原始时间格式：8月2号 11:39
+     * @param timestamp
+     * @return
+     */
+    public static String getTime3(String callTime,String timestamp){
+        List<String> data=Arrays.asList(StringUtils.split(callTime,"月号 :"));
+        Date date=new Date(Long.valueOf(timestamp));
+        Calendar calendar=Calendar.getInstance();
+        calendar.setTime(date);
+        int year=calendar.get(Calendar.YEAR);
+        int month=Integer.valueOf(data.get(0))-1;
+        int day=Integer.valueOf(data.get(1));
+        int hour=Integer.valueOf(data.get(2));
+        int minute=Integer.valueOf(data.get(3));
+        calendar.set(year,month,day,hour,minute);
+        //使用apache时间格式化工具
+        return DateFormatUtils.format(calendar.getTime(),PATTERN);
+    }
+
+
+    public static void main(String[] args) {
+        String time=DateFormatUtil.getTime("2 Agustus 11.39","1535096035916");
+        System.out.println(time);
+        time=DateFormatUtil.getTime2("August 2, 11:39","1535096035916");
+        System.out.println(time);
+        time=DateFormatUtil.getTime3("8月2号 11:39","1535096035916");
+        System.out.println(time);
+
+        Date date=new Date(Long.valueOf("1535342773638"));
+        Calendar calendar=Calendar.getInstance();
+        calendar.setTime(date);
+        System.out.println(DateFormatUtils.format(calendar.getTime(),PATTERN));
+        calendar.add(Calendar.DATE,-1);
+        calendar.add(Calendar.MINUTE,-120);
+        System.out.println(DateFormatUtils.format(calendar.getTime(),PATTERN));
+        System.out.println(-Integer.valueOf(20));
+    }
+}
